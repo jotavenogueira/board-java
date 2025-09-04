add49df5259d1bce041d9418694c14e16078d0b0
jotave <jotavenog@gmail.com>
2025-09-04 17:13:24 -0300

    arrumei o erro

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,12 @@ repositories {
 dependencies {
     implementation("org.liquibase:liquibase-core:4.29.1")
     implementation("mysql:mysql-connector-java:8.0.33")
-    implementation("org.projectlombok:lombok:1.18.34")
+    implementation("org.projectlombok:lombok:1.18.38")
 
-    annotationProcessor("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+}
+application {
+    mainClass.set("br.com.dio.Main")
 }
 
 
--- src/main/java/br/com/dio/Main.java
+++ src/main/java/br/com/dio/Main.java
@@ -12,7 +12,6 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
         try(var connection = getConnection()) {
-            System.out.println("Connected to the database!");
             new MigrationStrategy(connection).executeMigration();
         }
     }
